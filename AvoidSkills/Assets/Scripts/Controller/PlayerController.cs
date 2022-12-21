6b35c5f7f0b6302cd3f8cf99ec080a1c6d462d4f
MomDra <yem9087@koreatech.ac.kr>
2022-12-21 23:37:34 +0900

    Update PlayerController.cs
    namespace Network

--- AvoidSkills/Assets/Scripts/Controller/PlayerController.cs
+++ AvoidSkills/Assets/Scripts/Controller/PlayerController.cs
@@ -2,23 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Network
 {
-    private void FixedUpdate()
+    public class PlayerController : MonoBehaviour
     {
-        SendInputToServer();
-    }
-
-    private void SendInputToServer()
-    {
-        bool[] _inputs = new bool[]
+        private void FixedUpdate()
         {
+            SendInputToServer();
+        }
+
+        private void SendInputToServer()
+        {
+            bool[] _inputs = new bool[]
+            {
             Input.GetKey(KeyCode.W),
             Input.GetKey(KeyCode.S),
             Input.GetKey(KeyCode.A),
             Input.GetKey(KeyCode.D)
-        };
+            };
 
-        ClientSend.PlayerMovement(_inputs);
+            ClientSend.PlayerMovement(_inputs);
+        }
     }
 }
+
