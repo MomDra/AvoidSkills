dca2c845bbacfa7eb155bbc205c133b0be9a85fe
MomDra <yem9087@koreatech.ac.kr>
2022-12-21 20:31:15 +0900

    Move Player

--- Assets/Scripts/Network/ClientHandle.cs
+++ Assets/Scripts/Network/ClientHandle.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
 
-public class ClientHandle : MonoBehaviour
+public class ClientHandle
 {
 
     public static void Welcome(Packet _packet)
@@ -14,6 +15,7 @@ public class ClientHandle : MonoBehaviour
         Client.instance.myId = _myId;
 
         ClientSend.WelcomeReceived();
+        Client.instance.udp.Connect(((IPEndPoint)Client.instance.tcp.socket.Client.LocalEndPoint).Port);
     }
 
     public static void SpawnPlayer(Packet _packet)
@@ -23,27 +25,22 @@ public class ClientHandle : MonoBehaviour
         Vector3 _position = _packet.ReadVector3();
         Quaternion _rotation = _packet.ReadQuaternion();
 
-        if (GameManager.Instance == null)
-        {
-            Debug.Log("1@@");
-        }
-
-        if (_username == null)
-        {
-            Debug.Log("2@@");
-        }
-
-        if (_position == null)
-        {
-            Debug.Log("3@@");
-
-        }
-
-        if (_rotation == null)
-        {
-            Debug.Log("4@@");
-        }
-
         GameManager.Instance.SpawnPlayer(_id, _username, _position, _rotation);
+    }
+
+    public static void PlayerPosition(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Vector3 _position = _packet.ReadVector3();
+
+        GameManager.players[_id].transform.position = _position;
+    }
+
+    public static void PlayerRotation(Packet _packet)
+    {
+        int _id = _packet.ReadInt();
+        Quaternion _rotation = _packet.ReadQuaternion();
+
+        GameManager.players[_id].transform.rotation = _rotation;
     }
 }
--- Assets/Scripts/Network/ClientSend.cs
+++ Assets/Scripts/Network/ClientSend.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClientSend : MonoBehaviour
+public class ClientSend
 {
     private static void SendTCPData(Packet _packet)
     {
         _packet.WriteLength();
         Client.instance.tcp.SendData(_packet);
+    }
+
+    private static void SendUDPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.instance.udp.SendData(_packet);
     }
 
     #region Packets
@@ -21,5 +28,20 @@ public class ClientSend : MonoBehaviour
             SendTCPData(_packet);
         }
     }
-    #endregion 
+
+    public static void PlayerMovement(bool[] _inputs)
+    {
+        using (Packet _packet = new Packet((int)ClientPackets.playerMovement))
+        {
+            _packet.Write(_inputs.Length);
+            foreach (bool _input in _inputs)
+            {
+                _packet.Write(_input);
+            }
+            _packet.Write(GameManager.players[Client.instance.myId].transform.rotation);
+
+            SendUDPData(_packet);
+        }
+    }
+    #endregion
 }
