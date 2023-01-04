90f1c779cc9e4355562b468996d96fb466626d9d
MomDra <yem9087@koreatech.ac.kr>
2023-01-04 14:38:45 +0900

    Fall book
    connect server client

---
+++ AvoidSkillsServer/Assets/Scripts/Skill/SpecificSkills/FallBook/Cmd_FallBook_Lv2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cmd_FallBook_Lv2 : SkillCommand
+{
+    private static Cmd_FallBook_Lv2 instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    public override void cmd(Player _player, PlayerStatus _status, Vector3 _mousePos)
+    {
+        GameObject ob = Instantiate(skillInfo.skillPrefab, _mousePos + Vector3.up * 7, Quaternion.identity);
+        ob.GetComponent<Rigidbody>().velocity = Vector3.down * skillInfo.projectileSpeed;
+        ob.GetComponent<Projectile>().Initialize(_player.id, 1.5f, skillInfo.skillCode, skillInfo.level);
+    }
+}
